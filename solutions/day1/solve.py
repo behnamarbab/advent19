f4d717716c172760d0d183b18d1b13324a852bfe
Behnam Bojnordi Arbab <bbojnord@unb.ca>
2020-01-06 15:27:04 -0400

    Day1

---
+++ solutions/day1/solve.py
@@ -0,0 +1,31 @@
+def readinput(path):
+    f = open(path, 'r')
+    ret = f.read()
+    f.close()
+    return ret
+
+def solve_1(arr):
+    res = 0
+    for l in arr:
+        res += l//3-2
+    return res
+
+def solve_2(arr):
+    res = 0
+    for l in arr:
+        x = l
+        while x > 0:
+            x = x//3-2
+            res += max(x, 0)
+    return res
+
+if __name__=="__main__":
+    f = readinput('../../inputs/day1/a_1.txt')
+    f = [int(x) for x in f.split()]
+    sol1 = solve_1(f)
+    print("Result of A:", sol1)
+
+    f = readinput('../../inputs/day1/b_1.txt')
+    f = [int(x) for x in f.split()]
+    sol2 = solve_2(f)
+    print("Result of B:", sol2)
